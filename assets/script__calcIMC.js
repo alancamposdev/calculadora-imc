ff2d3ab6008e6d239726eacd6acb6844f64a2c99
AlanCampos <alancamposdeveloper@gmail.com>
2023-04-16 16:50:03 -0400

    refatorando

--- assets/script__calcIMC.js
+++ assets/script__calcIMC.js
@@ -1,65 +1,54 @@
 const btnCalculate = document.getElementById('button');
+btnCalculate.addEventListener('click', calculate);
 
-function calculate(){
-  if( mass !== '' && height !== '')
-  {
+function calculate() {
+  const mass = document.getElementById("mass").value;
+  const height = document.getElementById("height").value;
+  btnCalculate.addEventListener('click', openResponse);
 
-      const mass = document.getElementById("weight").value;
-      const height = document.getElementById("height").value;
-      const response = document.getElementById("response");
-      const imc = (mass / (height * height)).toFixed(1);
-      let classification = [
-        "Abaixo do peso",
-        "Peso ideal",
-        "Sobrepeso",
-        "Obesidade grau I",
-        "Obesidade grau II",
-        "Obesidade grau III",
-      ];
+  /*  validation   */
+  if (mass !== '' && height !== '') {
+    const response = document.getElementById("response");
+    const imc = (mass / (height * height)).toFixed(1);
+    const classification = [
+      "Abaixo do peso",
+      "Peso ideal",
+      "Sobrepeso",
+      "Obesidade grau I",
+      "Obesidade grau II",
+      "Obesidade grau III",
+    ];
 
-      function res(){
-        if( imc < 18.5){
-          response.textContent = 
-          `Seu IMC é de ${imc}. Você está  ${classification[0]}`
-        }
-        else if( imc < 25){
-          response.textContent = 
-          `Seu IMC é de ${imc}. Você está no ${classification[1]}`
-        }
-        else if( imc < 30){
-          response.textContent = 
-          `Seu IMC é de ${imc}. Você está com ${classification[2]}`
-        }
-        else if( imc < 35){
-          response.textContent = 
-          `Seu IMC é de ${imc}. Você está com ${classification[3]}`
-        }
-        else if( imc < 40){
-          response.textContent = 
-          `Seu IMC é de ${imc}. Você está com ${classification[4]}`
-        }
-        else if( imc > 40){
-          response.textContent = 
-          `Seu IMC é de ${imc}. Você está com${classification[5]}`
-        }
-      }res();
+    // Return 
+    const answer = function () {
+      if (imc < 18.5) {
+        response.textContent =
+          `Seu IMC é de ${imc}. Você está  ${classification[0]}`;
+      } else if (imc < 25) {
+        response.textContent =
+          `Seu IMC é de ${imc}. Você está no ${classification[1]}`;
+      } else if (imc < 30) {
+        response.textContent =
+          `Seu IMC é de ${imc}. Você está com ${classification[2]}`;
+      } else if (imc < 35) {
+        response.textContent =
+          `Seu IMC é de ${imc}. Você está com ${classification[3]}`;
+      } else if (imc < 40) {
+        response.textContent =
+          `Seu IMC é de ${imc}. Você está com ${classification[4]}`;
+      } else {
+        response.textContent =
+          `Seu IMC é de ${imc}. Você está com ${classification[5]}`;
+      }
+    }();
 
-  }else
-    {
-      response.textContent = " Preencha todos os campos ";
-    } 
+  } else {
+    response.style.color = "red";
+    response.innerHTML = " Preencha todos os campos ! ";
+  }
 }
 
-function openResponse(){
+function openResponse() {
   response.style.background = "#E8F0FE";
   response.style.minHeight = "80px";
-
 }
-
-btnCalculate.addEventListener('click',calculate);
-btnCalculate.addEventListener('click', openResponse);
-
- 
-   
-  
-
